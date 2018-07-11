5a5b7589c15ab31e2e642308ab3bfe1ac94f0dda
baptistefkt <baptistefirket@gmail.com>
2018-07-11 17:00:02 +0200

    data2 ok

--- challenge-js-files/assets/script.js
+++ challenge-js-files/assets/script.js
@@ -1,17 +1,14 @@
-// function construct(c,y,i){
-//     this.country = c;
-//     this.year = y;
-//     this.infractions = i;
-// }
-let data = [];
-let tableRows1 = document.getElementById('table1').rows;
-//console.log(tableRows1[1].cells);
-for (let i = 1; i < tableRows1.length; i++){
-    // console.log(tableRows1[i].cells.length);
-    for (let j = 1; j < tableRows1[i].cells.length; j++){
-        if ((tableRows1[i].cells[1].innerText !== "")&&(tableRows1[1].cells[j].innerText !== "")){
+// tableau 1
 
-        data.push(
+let data1 = [];
+let tableRows1 = document.getElementById('table1').rows;
+
+for (let i = 1; i < tableRows1.length; i++){
+
+    for (let j = 1; j < tableRows1[i].cells.length; j++){
+        if ((tableRows1[i].cells[1].innerText !== "")&&(tableRows1[1].cells[j].innerText !== "")&&(tableRows1[i].cells[j].innerText !== ":")){
+
+        data1.push(
             {
                 "country": tableRows1[i].cells[1].innerText,
                 "year": tableRows1[1].cells[j].innerText,
@@ -21,10 +18,45 @@ for (let i = 1; i < tableRows1.length; i++){
         }
     }
 
-console.log(data);
-// var svg = dimple.newSvg("body", 600, 400);
-// var myChart = new dimple.chart(svg, data);
-// myChart.addCategoryAxis("x", "year");
-// myChart.addMeasureAxis("y", "infractions");
-// myChart.addSeries("country", dimple.plot.line);
-// myChart.draw();
+// tableau 2
+
+let data2 = [];
+let table2 = document.getElementById('table2');
+let tableRows2 = table2.rows;
+
+for (let i = 0; i <tableRows2.length; i++){
+    for (let j = 1; j < tableRows2[i].cells.length; j++){
+        if ((tableRows2[i].cells[1].innerText !== "Pays")&&(tableRows2[0].cells[j].innerText !== "Pays")){
+            data2.push(
+                {
+                    "country": tableRows2[i].cells[1].innerText,
+                    "year": tableRows2[0].cells[j].innerText,
+                    "population": tableRows2[i].cells[j].innerText
+                }
+            )
+        }
+    }
+}
+// create div table1
+
+function makeDiv1(){
+
+}
+
+
+let svg1 = dimple.newSvg("#chartContainer1", 800, 600);
+let myChart1 = new dimple.chart(svg1, data1);
+var myLegend1 = myChart1.addLegend(200, 140, 500, 500);
+myChart1.addCategoryAxis("x", "year");
+myChart1.addMeasureAxis("y", "infractions");
+myChart1.addSeries("country", dimple.plot.line);
+myChart1.draw();
+
+
+let svg2 = dimple.newSvg("#chartContainer2", 800, 600);
+let myChart2 = new dimple.chart(svg2, data2);
+var myLegend2 = myChart2.addLegend(200, 140, 500, 500);
+myChart2.addCategoryAxis("x", "year");
+myChart2.addMeasureAxis("y", "population");
+myChart2.addSeries("country", dimple.plot.bar);
+myChart2.draw();
